be2cea2de2cb54a0ad90fdc2d1b4c07be10adf9b
vonssy <kak.reyvaldizakaria@gmail.com>
2025-05-17 05:09:42 +0700

    Added Rotate Proxies Feature

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from aiohttp_socks import ProxyConnector
 from fake_useragent import FakeUserAgent
 from datetime import datetime, timezone
 from colorama import *
-import asyncio, os, re, pytz
+import asyncio, time, json, base64, os, re, pytz
 
 wib = pytz.timezone('Asia/Jakarta')
 
@@ -120,19 +120,53 @@ class BillionsNetwork:
 
         return datetime.fromisoformat(date_str)
     
+    def get_token(self, session_id: str):
+        try:
+            token = session_id.replace("session_id=", "")
+
+            return token
+        except Exception as e:
+            return None
+        
+    def decode_token(self, token: str):
+        try:
+            header, payload, signature = token.split(".")
+            decoded_payload = base64.urlsafe_b64decode(payload + "==").decode("utf-8")
+            parsed_payload = json.loads(decoded_payload)
+            exp_time = parsed_payload["exp"]
+            
+            return exp_time
+        except Exception as e:
+            return None
+        
+    def check_token_status(self, token: str):
+        try:
+            exp_time = self.decode_token(token)
+            now = int(time.time())
+
+            if now > exp_time:
+                return False
+
+            return True
+        except Exception as e:
+            return False
+    
     def mask_account(self, account):
         if "@" in account:
             local, domain = account.split('@', 1)
             mask_account = local[:3] + '*' * 3 + local[-3:]
             return f"{mask_account}@{domain}"
         
+        mask_account = local[:6] + '*' * 6 + local[-6:]
+        return mask_account
+        
     def print_question(self):
         while True:
             try:
-                print("1. Run With Monosans Proxy")
-                print("2. Run With Private Proxy")
-                print("3. Run Without Proxy")
-                choose = int(input("Choose [1/2/3] -> ").strip())
+                print(f"{Fore.WHITE + Style.BRIGHT}1. Run With Monosans Proxy{Style.RESET_ALL}")
+                print(f"{Fore.WHITE + Style.BRIGHT}2. Run With Private Proxy{Style.RESET_ALL}")
+                print(f"{Fore.WHITE + Style.BRIGHT}3. Run Without Proxy{Style.RESET_ALL}")
+                choose = int(input(f"{Fore.BLUE + Style.BRIGHT}Choose [1/2/3] -> {Style.RESET_ALL}").strip())
 
                 if choose in [1, 2, 3]:
                     proxy_type = (
@@ -141,17 +175,30 @@ class BillionsNetwork:
                         "Run Without Proxy"
                     )
                     print(f"{Fore.GREEN + Style.BRIGHT}{proxy_type} Selected.{Style.RESET_ALL}")
-                    return choose
+                    break
                 else:
                     print(f"{Fore.RED + Style.BRIGHT}Please enter either 1, 2 or 3.{Style.RESET_ALL}")
             except ValueError:
                 print(f"{Fore.RED + Style.BRIGHT}Invalid input. Enter a number (1, 2 or 3).{Style.RESET_ALL}")
+
+        rotate = False
+        if choose in [1, 2]:
+            while True:
+                rotate = input(f"{Fore.BLUE + Style.BRIGHT}Rotate Invalid Proxy? [y/n] -> {Style.RESET_ALL}").strip()
+
+                if rotate in ["y", "n"]:
+                    rotate = rotate == "y"
+                    break
+                else:
+                    print(f"{Fore.RED + Style.BRIGHT}Invalid input. Enter 'y' or 'n'.{Style.RESET_ALL}")
+
+        return choose, rotate
     
     async def check_connection(self, proxy=None):
         connector = ProxyConnector.from_url(proxy) if proxy else None
         try:
             async with ClientSession(connector=connector, timeout=ClientTimeout(total=30)) as session:
-                async with session.get(url=self.BASE_API, headers={}) as response:
+                async with session.get(url="https://unich.com", headers={}) as response:
                     response.raise_for_status()
                     return True
         except (Exception, ClientResponseError) as e:
@@ -196,7 +243,16 @@ class BillionsNetwork:
                     continue
                 return None
             
-    async def process_check_connection(self, session_id: str, use_proxy: bool):
+    async def process_check_connection(self, session_id: str, token: str, use_proxy: bool, rotate_proxy: bool):
+        is_token_active = self.check_token_status(token)
+        if not is_token_active:
+            self.log(
+                f"{Fore.CYAN+Style.BRIGHT}Session Id:{Style.RESET_ALL}"
+                f"{Fore.BLUE+Style.BRIGHT} {self.mask_account(token)} {Style.RESET_ALL}"
+                f"{Fore.RED+Style.BRIGHT}Expired{Style.RESET_ALL}"
+            )
+            return False
+        
         message = "Checking Connection, Wait..."
         if use_proxy:
             message = "Checking Proxy Connection, Wait..."
@@ -210,6 +266,31 @@ class BillionsNetwork:
         )
 
         proxy = self.get_next_proxy_for_account(session_id) if use_proxy else None
+
+        if rotate_proxy:
+            is_valid = None
+            while is_valid is None:
+                is_valid = await self.check_connection(proxy)
+                if not is_valid:
+                    self.log(
+                        f"{Fore.CYAN+Style.BRIGHT}Proxy   :{Style.RESET_ALL}"
+                        f"{Fore.WHITE+Style.BRIGHT} {proxy} {Style.RESET_ALL}"
+                        f"{Fore.MAGENTA+Style.BRIGHT}-{Style.RESET_ALL}"
+                        f"{Fore.RED+Style.BRIGHT} Not 200 OK, {Style.RESET_ALL}"
+                        f"{Fore.YELLOW+Style.BRIGHT}Rotating Proxy...{Style.RESET_ALL}"
+                    )
+                    proxy = self.rotate_proxy_for_account(session_id) if use_proxy else None
+                    await asyncio.sleep(5)
+                    continue
+
+                self.log(
+                    f"{Fore.CYAN+Style.BRIGHT}Proxy   :{Style.RESET_ALL}"
+                    f"{Fore.WHITE+Style.BRIGHT} {proxy} {Style.RESET_ALL}"
+                    f"{Fore.MAGENTA+Style.BRIGHT}-{Style.RESET_ALL}"
+                    f"{Fore.GREEN+Style.BRIGHT} 200 OK {Style.RESET_ALL}                  "
+                )
+
+                return True
 
         is_valid = await self.check_connection(proxy)
         if not is_valid:
@@ -230,8 +311,8 @@ class BillionsNetwork:
 
         return True
         
-    async def process_accounts(self, session_id: str, use_proxy: bool):
-        is_valid = await self.process_check_connection(session_id, use_proxy)
+    async def process_accounts(self, session_id: str, token: str, use_proxy: bool, rotate_proxy: bool):
+        is_valid = await self.process_check_connection(session_id, token, use_proxy, rotate_proxy)
         if is_valid:
             proxy = self.get_next_proxy_for_account(session_id) if use_proxy else None
 
@@ -305,7 +386,7 @@ class BillionsNetwork:
             with open('cookies.txt', 'r') as file:
                 session_ids = [line.strip() for line in file if line.strip()]
             
-            use_proxy_choice = self.print_question()
+            use_proxy_choice, rotate_proxy = self.print_question()
 
             while True:
                 use_proxy = False
@@ -325,15 +406,18 @@ class BillionsNetwork:
                 separator = "=" * 26
                 for idx, session_id in enumerate(session_ids, 1):
                     if session_id:
-                        self.log(
-                            f"{Fore.CYAN + Style.BRIGHT}{separator}[{Style.RESET_ALL}"
-                            f"{Fore.WHITE + Style.BRIGHT} {idx} {Style.RESET_ALL}"
-                            f"{Fore.CYAN + Style.BRIGHT}Of{Style.RESET_ALL}"
-                            f"{Fore.WHITE + Style.BRIGHT} {len(session_ids)} {Style.RESET_ALL}"
-                            f"{Fore.CYAN + Style.BRIGHT}]{separator}{Style.RESET_ALL}"
-                        )
-                        await self.process_accounts(session_id, use_proxy)
-                        await asyncio.sleep(3)
+                        token = self.get_token(session_id)
+
+                        if token:
+                            self.log(
+                                f"{Fore.CYAN + Style.BRIGHT}{separator}[{Style.RESET_ALL}"
+                                f"{Fore.WHITE + Style.BRIGHT} {idx} {Style.RESET_ALL}"
+                                f"{Fore.CYAN + Style.BRIGHT}Of{Style.RESET_ALL}"
+                                f"{Fore.WHITE + Style.BRIGHT} {len(session_ids)} {Style.RESET_ALL}"
+                                f"{Fore.CYAN + Style.BRIGHT}]{separator}{Style.RESET_ALL}"
+                            )
+                            await self.process_accounts(session_id, token, use_proxy, rotate_proxy)
+                            await asyncio.sleep(3)
 
                 self.log(f"{Fore.CYAN + Style.BRIGHT}={Style.RESET_ALL}"*63)
                 seconds = 24 * 60 * 60
